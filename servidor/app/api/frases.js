60ea6130888c9e2afae0ad24eba27d2a79a3fde5
Douglas Ribeiro <ribeiro.douglas5052@gmail.com>
2023-01-26 21:36:34 -0300

    sentence correction

--- servidor/app/api/frases.js
+++ servidor/app/api/frases.js
@@ -1,16 +1,16 @@
 var api = {};
 
 var frases = [
-	{_id: 0, texto:'Alura, Cursos online de tecnologia que reinventam sua carreira.', tempo: 15 },
-	{_id: 1, texto:'Debuggers não consertam erros, apenas os exibem em slow motion.',tempo: 8 },
-	{_id: 2, texto:'Caelum, Ensino e Inovação.', tempo: 5 },
-	{_id: 3, texto:'Existem duas tarefas difíceis na Ciência da Computação: invalidação de cache e nomear as coisas.', tempo: 15 },
-	{_id: 4, texto:'Ciência da computação é tão sobre computadores quanto astronomia é sobre telescópios.', tempo: 15 },
-	{_id: 5, texto:'Na minha máquina funciona.', tempo: 5 },
-	{_id: 6, texto:'Hardware é o que você chuta, software é o que você xinga.', tempo: 12 },
-	{_id: 7, texto:'Software em funcionamento mais que documentação abrangente.', tempo: 10 },
-	{_id: 8, texto:'Iterar é humano, recursão é divino.', tempo: 7},
-	{_id: 9, texto:'Existem três jeitos de desenvolver software. O jeito certo, o jeito errado e o meu jeito, que é igual o jeito errado só que mais rápido.', tempo: 20},
+	{_id: 0, texto:'A primeira foto do mundo foi capturada ao longo de 8h.', tempo: 12 },
+	{_id: 1, texto:'O e-mail foi criado antes da internet.',tempo: 6 },
+	{_id: 2, texto:'A primeira página da internet continua online.', tempo: 8 },
+	{_id: 3, texto:'Ogivas nucleares ficaram protegidas por anos com a senha 00000000.', tempo: 10 },
+	{_id: 4, texto:'Em 1956, um HD de 5 Mb pesava uma tonelada.', tempo: 5 },
+	{_id: 5, texto:'A primeira pessoa a programar foi uma mulher: Ada Lovelace.', tempo: 8 },
+	{_id: 6, texto:'Um computador tão poderoso quanto o cérebro humano seria capaz de realizar 38.000 trilhões de operações por segundo.', tempo: 17 },
+	{_id: 7, texto:'Creeper, escrito por Bob Thomas em 1971, é o primeiro vírus de computador.', tempo: 10 },
+	{_id: 8, texto:'Steve Jobs construiu o primeiro computador Apple com peças de reposição de seu antigo empregador.', tempo: 14},
+	{_id: 9, texto:'Atualmente, existem mais de 17 bilhões de dispositivos conectados à internet.', tempo: 11},
 
 	];
 
